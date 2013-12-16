128939982ee51255ca0e701c046896bb9404734d
Valtis <erkka_kaaria@outlook.com>
2013-12-16 17:07:25 +0200

    String tokenizer

---
+++ AsmVM/StringUtility.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+#include <string>
+
+namespace Utility
+{
+  std::vector<std::string> Tokenize(std::string text, std::string delimiters);
+  std::vector<std::string> HandleControlCharacters(std::string line);
+  std::vector<std::string> WrapLines(std::vector<std::string> lines, double lineWidth, double characterWidth);
+}
